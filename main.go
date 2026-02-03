a622292eb1c09da283702d7101aada3020a57c8a
udaykiran015 <peddiudaykiran61@gmail.com>
2026-02-03 22:16:24 +0530

    resent

--- main.go
+++ main.go
@@ -6,13 +6,11 @@ import (
 )
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0.1, 0.2, 0.15}
-	result := make(map[float64][]float64)
 
 	for _, taxrate := range taxRates {
-		pricejob := prices.NewTaxIncludePriceJob(taxrate).Process()
-	}
+		priceJob :=prices.NewTaxIncludePriceJob(taxrate)
+		priceJob.Process()	}
 	
 
 }
--- prices/prices.go
+++ prices/prices.go
@@ -7,15 +7,14 @@ type TaxIncludePriceJob struct {
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
 }
-
 func (job TaxIncludePriceJob) Process() {
 	result := make(map[string]float64)
 
 	for _, price := range job.InputPrices {
-		result[fmt.Sprintf("%.2f",price)] = price * (1 + job.TaxRate)
+		result[fmt.Sprintf("%.2f", price)] = price * (1 + job.TaxRate)
 
 	}
-	
+	fmt.Println(result)
 
 }
 
